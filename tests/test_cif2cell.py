1cdd3d2f5415c275a9b448237ae073bf9171aab6
Koki Muraoka <koki.muraoka@gmail.com>
2018-05-04 02:12:02 +0900

    Use glob instead of pathlib

--- tests/test_cif2cell.py
+++ tests/test_cif2cell.py
@@ -1,10 +1,10 @@
 import unittest
 import subprocess
-import pathlib
+import glob
 
 
 class TestCif2Cell(unittest.TestCase):
     def test_cif(self):
-        for path in pathlib.Path('cifs').glob("*.cif"):
+        for path in glob.glob('cifs/*.cif'):
             out = subprocess.check_output(['./cif2cell', path])
 
