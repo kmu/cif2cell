3cb5968694975c63c3adff0d88420abd39063e7e
Koki Muraoka <koki.muraoka@gmail.com>
2018-05-04 01:39:29 +0900

    Minor fix

--- tests/test_cif2cell.py
+++ tests/test_cif2cell.py
@@ -7,5 +7,4 @@ class TestCif2Cell(unittest.TestCase):
     def test_cif(self):
         for path in pathlib.Path('cifs').glob("*.cif"):
             out = subprocess.check_output(['cif2cell', path])
-            print(out)
 
