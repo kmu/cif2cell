ff835ec4b40fa1092474b72a6c2c4db3b3a7c421
Koki Muraoka <koki.muraoka@gmail.com>
2018-12-21 14:25:10 +0900

    Test to check vasp issue

--- tests/test_cif2cell.py
+++ tests/test_cif2cell.py
@@ -12,3 +12,14 @@ class TestCif2Cell(unittest.TestCase):
             print(stdout_data.decode())
 
             assert not "***Warning: Space group operation check failed" in stdout_data.decode()
+            assert not "Error" in stdout_data.decode()
+
+    def test_vasp(self):
+        # for path in glob.glob('cifs/*.cif'):
+        p = subprocess.Popen(['./cif2cell', "-p", "vasp", "-f", 'cifs/Si.cif'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout_data, stderr_data = p.communicate()
+        print("OUT")
+        print(stdout_data.decode())
+
+        assert not "***Warning: Space group operation check failed" in stdout_data.decode()
+        assert not "Error" in stdout_data.decode()
